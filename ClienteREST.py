33801ca1b95c9e2ed7c3eb405c0b8b85ca3232f2
Diego Murguía <220300724@ucaribe.edu.mx>
2026-05-10 11:28:52 -0500

    Primer y único commit (supongo) de la actividad 2

---
+++ ClienteREST.py
@@ -0,0 +1,33 @@
+import requests
+
+# =========================
+# FASE 2: CLIENTE REST
+# =========================
+
+# URL de la API REST
+url = "https://jsonplaceholder.typicode.com/users"
+
+# Realizar petición HTTP GET
+respuesta = requests.get(url)
+
+# Convertir JSON a estructuras nativas de Python
+usuarios = respuesta.json()
+
+
+# =========================
+# FASE 3: PROCESAMIENTO E INTELIGENCIA ORGANIZACIONAL
+# =========================
+
+print("Informe de empleados\n")
+
+# Recorrer lista de usuarios
+for usuario in usuarios:
+
+    # Extraer únicamente los datos relevantes
+    nombre = usuario["name"]
+    empresa = usuario["company"]["name"]
+
+    # Mostrar informe limpio
+    print(f"Empleado: {nombre}")
+    print(f"Empresa: {empresa}")
+    print("-" * 40)
